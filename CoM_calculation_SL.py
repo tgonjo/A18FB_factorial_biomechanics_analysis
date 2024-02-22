b454c157f5c1c42285e292361959c8a9209dc59b
tgonjo <113050191+tgonjo@users.noreply.github.com>
2024-02-22 17:07:54 +0000

    Update CoM_calculation_SL.py - axis minor updates

--- CoM_calculation_SL.py
+++ CoM_calculation_SL.py
@@ -193,8 +193,8 @@ with st.form(key='upload_form'):
         fig,ax = plt.subplots()
         ax.plot(df_raw['Time']-df_raw['Time'].iloc[0], df_raw['HipY'])
         ax.set_title('This is your HipY time-series plot. Does the time and displacement values look like seconds and metres?')
-        ax.set_ylabel('Time')
-        ax.set_xlabel('Displacement')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Displacement')
         st.pyplot(fig)
         st.text('If either the time or displacement unit seems wrong, go back to your Excel file and fix it')
         
